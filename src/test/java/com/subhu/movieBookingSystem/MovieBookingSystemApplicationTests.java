8a03a0c2a9a31e6a9064361f9ec29f65047be60b
i-am-subhasish <dassubhasish96@gmail.com>
2021-08-06 19:54:45 +0530

    initial files

---
+++ src/test/java/com/subhu/movieBookingSystem/MovieBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhu.movieBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
